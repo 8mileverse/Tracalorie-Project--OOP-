21ab5e1b45f25ea5507d109ba13af7a89d58593e
8mileverse <Ayodelu4@gmail.com>
2024-12-05 07:33:52 +0100

    oop-Display Stats Features

--- js/app.js
+++ js/app.js
@@ -1,21 +1,68 @@
 class calorieTracker {
   constructor() {
+    // runs the function wjen it is intanstiated
     this._calorielimit = 3000;
     this._totalLimit = 0;
     this._meals = []; // adds to the calorie
     this._workouts = []; // takes away from the calorie
+    this._displayCaloriesLimit();
+    this._displayCaloriesTotal(); // Display initial calories total on page load
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
   }
 
+  // Public Methods/Api --------------------------------
   addMeal(meal) {
     // to the meal list above
     this._meals.push(meal);
     this._totalLimit += meal.calories;
+    this._render();
   }
 
   addWorkout(workouts) {
     // to the workout list above
     this._workouts.push(workouts);
     this._totalLimit -= workouts.calories;
+    this._render();
+  }
+
+  // Private Methods --------------------------------
+  _displayCaloriesTotal() {
+    const totalCalEl = document.querySelector("#calories-total");
+    totalCalEl.innerHTML = this._totalLimit;
+  }
+
+  _displayCaloriesLimit() {
+    const totalCalLi = document.querySelector("#calories-limit");
+    totalCalLi.innerHTML = this._calorielimit;
+  }
+
+  _displayCaloriesConsumed() { // calories gained
+
+    const totalCalConsumedLi = document.getElementById('calories-consumed');
+
+    const consumed = this._meals.reduce((total, meal) => total + meal.calories, 0);
+    totalCalConsumedLi.innerHTML = consumed;
+  }
+
+  _displayCaloriesBurned() { // calories lost
+
+    const totalCalBurnedLi = document.getElementById('calories-burned');
+
+    const burned = this._workouts.reduce((total, workout) => total + workout.calories, 0);
+    totalCalBurnedLi.innerHTML = burned;
+  }
+_displayCaloriesRemaining(){
+  const totalCalRemainingLi = document.getElementById('calories-remaining');
+  const remaining = this._calorielimit - this._totalLimit;
+  totalCalRemainingLi.innerHTML = remaining;
+}
+  _render() {
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
   }
 }
 class Meal {
@@ -36,8 +83,8 @@ class Workout {
 
 const tracker = new calorieTracker();
 
-const breakfast = new Meal('Breakfast', 400);
-const fatBurn = new Workout('Pushup', 500 )
+const breakfast = new Meal("Breakfast", 900);
+const fatBurn = new Workout("Pushup", 500);
 
 tracker.addMeal(breakfast);
 tracker.addWorkout(fatBurn);
